a27cacf2e510b23626fe6d01a0e8bf562261579f
Redmond Chan <redmondchan92@gmail.com>
2019-01-06 18:56:16 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,7 +64,7 @@ def all_supplies_in_holidays(holiday_hash)
       array = name.to_s.split("_")
       array.each {|word| new_array << word.capitalize!}
       name = new_array.join(" ")
-      supply = supply.join(",")
+      supply = supply.join(", ")
       puts "  #{name}: #{supply}"
     end
   end
